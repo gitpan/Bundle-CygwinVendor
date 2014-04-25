b5edc8844ac2809e2695faae49186e1418c6d791
Reini Urban <rurban@x-ray.at>
2014-04-25 15:44:01 +0000

    Import of RURBAN/Bundle-CygwinVendor-0.02 from CPAN.
    gitpan-cpan-distribution: Bundle-CygwinVendor
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         RURBAN/Bundle-CygwinVendor-0.02.tar.gz
    gitpan-cpan-author:       RURBAN
    gitpan-cpan-maturity:     released

--- CygwinVendor.pm
+++ CygwinVendor.pm
@@ -1,6 +1,6 @@
 package Bundle::CygwinVendor;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 1;
 
@@ -18,153 +18,163 @@ For all Perl versions:
 
 =head1 CONTENTS
 
-Pod::Escapes	1.04
+Pod::Escapes		1.06
 
-Pod::Simple	3.14
+Pod::Simple		3.28
 
-Test::Pod	1.41
+Test::Pod		1.48
 
-Devel::Symdump	2.08
+Devel::Symdump		2.11
 
-Pod::Coverage	0.21
+Pod::Coverage		0.23
 
 Test::Pod::Coverage	1.08
 
-Compress::Raw::Bzip2	2.030
+Compress::Raw::Bzip2	2.064
 
-IO::Compress	2.030
+IO::Compress::Bzip2	2.064
 
-Compress::Bzip2	2.09
+Compress::Bzip2		2.17
 
-IO::String	1.08	
+IO::String		1.08
 
-Archive::Zip	1.30
+Archive::Zip		1.37
 
-Term::ReadKey	2.30
+Term::ReadKey		2.31
 
 Term::ReadLine::Perl	1.0303
 
-Term::ReadLine::Gnu	1.19
+Term::ReadLine::Gnu	1.24
 
-XML::NamespaceSupport	1.10
+XML::NamespaceSupport	1.11
 
-XML::SAX	0.96
+XML::SAX::Base		1.08
 
-XML::LibXML	1.70
+XML::SAX		0.99
 
-XML::Parser	2.36
+XML::LibXML		2.0116
 
-Proc::ProcessTable	0.45
+XML::Parser		2.41
 
-YAML	0.71
+Proc::ProcessTable	0.50
 
-Config::Tiny	2.12
+YAML			0.71
+
+YAML::LibYAML		0.41
+
+Config::Tiny		2.20
 
 File::Copy::Recursive	0.38
 
-IPC::Run3	0.044
+IPC::Run3		0.048
 
-Probe::Perl	0.01
+Probe::Perl		0.03
 
-Tee	0.13
+Tee			0.14
 
-IO::CaptureOutput	1.1102
+IO::CaptureOutput	1.1103
 
-File::pushd	1.00
+File::pushd		1.006
 
-File::HomeDir	0.89
+File::HomeDir		1.00
 
-Digest::SHA	5.48
+Digest::SHA		5.89
 
-Module::Signature	0.61
+Module::Signature	0.73
 
-URI	1.54
+URI			1.60
 
-HTML::Tagset	3.20
+HTML::Tagset		3.20
 
-HTML::Parser	3.64
+HTML::Parser		3.71
 
-LWP	5.836
+LWP			6.06
 
-CPAN	1.9402
+CPAN			2.05
 
-Net::IP	1.25
+Net::IP			1.26
 
-Net::DNS	0.66
+Net::DNS		0.74
 
-Test::Reporter	1.57
+Test::Reporter		1.60
 
-Crypt::SSLeay	0.58
+Net::SSLeay		1.58
 
-common::sense	3.3
+IO::Socket::SSL		1.981
 
-JSON::XS	2.3
+LWP::Protocol::https	6.04
 
-JSON		2.22
+common::sense		3.72
 
-Metabase::Client::Simple	0.008
+Types::Serialiser	1.0
 
-Data::UUID	1.215
+JSON::XS		3.01
 
-Data::GUID	0.045
+JSON			2.90
 
-CPAN::DistnameInfo	0.10
+Metabase::Client::Simple 0.009
 
-Metabase::Fact	0.017
+Data::UUID		1.219
 
-Config::Perl::V	0.12
+Data::GUID		0.048
+
+CPAN::DistnameInfo	0.12
+
+Metabase::Fact		0.024
+
+Config::Perl::V		0.20
 
 CPAN::Testers::Report	1.999001
 
 Test::Reporter::Transport::Metabase	1.999008
 
-CPAN::Reporter	1.1800
+CPAN::Reporter		1.2011
 
-Text::Glob	0.08
+Text::Glob		0.09
 
-Number::Compare	0.01
+Number::Compare		0.03
 
-File::Find::Rule	0.32
+File::Find::Rule	0.33
 
-Data::Compare	1.22
+Data::Compare		1.24
 
-CPAN::Checksums	2.04
+CPAN::Checksums		2.09
 
-File::Remove	1.42
+File::Remove		1.52
 
-File::chmod	0.32
+File::chmod		0.40
 
-Params::Util	1.01
+Params::Util		1.07
 
-Test::Script	1.07
+Test::Script		1.07
 
-CPAN::Inject	0.11
+CPAN::Inject		1.14
 
-Net::Telnet	3.03
+Net::Telnet		3.04
 
-Module::ScanDeps	0.98
+Module::ScanDeps	1.13
 
-PAR::Dist		0.47
+PAR::Dist		0.49
 
-ExtUtils::CBuilder	0.2702
+ExtUtils::CBuilder	0.280216
 
-ExtUtils::ParseXS	2.2206
+ExtUtils::ParseXS	3.24
 
-YAML::Tiny		1.44
+Software::License	0.103010
 
-Software::License	0.102340
+Module::Build		0.4205
 
-Module::Build	0.36_13
+Socket6			0.25
 
-Socket6		0.23
+IO::Socket::INET6	2.72
 
-IO::Socket::INET6	2.65
+IO::Socket::IP		0.29
 
-B::Generate	1.29
+B::Generate		1.48
 
-PadWalker	1.92
+PadWalker		1.98
 
-Alias		2.32
+Data::Alias		1.18
 
 
 =head1 DESCRIPTION
